0848bb65e7b85aa9692621cf4b9fdf847c999adb
Crowdbotics <team@crowdbotics.com>
2024-02-05 20:31:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "workforcescheduling-45983",
+  url: "https://workforcescheduling-45983.botics.co",
+  api: "https://workforcescheduling-45983.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
